3716a9f05c1dce4a22d8d4be57cef9d754d79d01
Carter Hemington <carter.hemington@gmail.com>
2022-01-10 21:34:28 +0000

    assertion and emojis

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+// function
+const assertEqual = function(actual,expected) {
+  if (actual === expected) {
+    console.log("😂😂😂 " + "Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("😈😈😈 " + "Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+//testing
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1,1);
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual(1,2);
