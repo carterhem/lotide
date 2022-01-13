534ead022d429335ed4d515bd9b0e89350e47a53
Carter Hemington <carter.hemington@gmail.com>
2022-01-13 02:22:48 +0000

    it works but i don't think it should

---
+++ findKeyByValue.js
@@ -0,0 +1,35 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`😂😂😂 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😈😈😈 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function (object, value) { // converting the object to an array
+  let objectArray = Object.keys(object)
+  // console.log(objectArray)
+  for (const key of objectArray){
+    if(object[key]===value){
+      return key;
+    }
+  }
+    // console.log key);
+    // if (objectArray[] === value) {
+    //   console.log(value);
+    //   console.log(objectArray key])
+    // }
+    
+
+  }
+
+//test cases
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
