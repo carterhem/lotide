6e3fdd85989fcf2b2ee293bc35b193b7121aab13
Carter Hemington <carter.hemington@gmail.com>
2022-01-17 15:06:20 +0000

    went smoothly, forgot to run first

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual,expected) {
-  if (actual === expected) {
-    console.log(`😂😂😂 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`😈😈😈 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function (actual, expected) {
  if (JSON.stringify(actual)===JSON.stringify(expected)){
    return true;
@@ -14,6 +6,5 @@ const eqArrays = function (actual, expected) {
  }
 }
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [1, 2, -4]), true); // => should FAIL
-assertEqual(eqArrays([1, 2, 3], [1, 2, -4]), false); // => should PASS
+module.exports = eqArrays;
+
