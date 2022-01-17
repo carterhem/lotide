eedcdb19c77bc431e3966c2999e50955ffc56fc5
Carter Hemington <carter.hemington@gmail.com>
2022-01-17 18:30:38 +0000

    fixed copies to be exporting

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -14,6 +14,5 @@ const eqArrays = function (actual, expected) {
    }
  }
 
+ module.exports = assertArraysEqual;
 
- assertArraysEqual([1, 2, 3], [1, 2, 3]);
- assertArraysEqual([1, 2, 3], [1, 2, 4]);
